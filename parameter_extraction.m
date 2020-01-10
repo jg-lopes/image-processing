7c4a25e77e9557d553a929d3242d801391d8d0c5
João Gabriel Lopes <jglopes6@gmail.com>
2020-01-10 09:46:05 +0100

    Adding extra files

---
+++ parameter_extraction.m
@@ -0,0 +1,35 @@
+function parameters = parameter_extraction(image)
+    
+    % Values of N analysed in the visu_contour file
+    BW = imbinarize(image);
+    B = bwboundaries(BW);
+    z=B{1}(:,1)+1i*B{1}(:,2);
+    coeff = dfdir(z, 10);
+    
+    % Module of the coeff
+    z = abs(coeff);
+    size(z)
+    
+    % Region props
+    stats = regionprops(BW, 'all');
+    %filtered_stats = stats;
+    % Difficult to encode data, wouldn't give the scalar values that might
+    % be useful for pca
+    filtered_stats = rmfield(stats, {'SubarrayIdx', 'ConvexHull','ConvexImage','ConvexHull','ConvexImage','Image','FilledImage','PixelIdxList','PixelList'});
+    % Multidimensional data that might be useful for PCA, but would require
+    % further dimensional treatment
+    %filtered_stats = rmfield(filtered_stats, {'Centroid', 'BoundingBox', 'Extrema', 'MaxFeretCoordinates', 'MinFeretCoordinates'});
+    cell = struct2cell(filtered_stats);
+    %cell;
+    
+    parameters = z';
+    %parameters = [];
+    for k = 1:length(cell)
+        cell{k} = cell{k}(:);
+        
+        for i = 1:length(cell{k})
+            parameters = [parameters cell{k}(i)];
+        end
+    end
+    
+end
