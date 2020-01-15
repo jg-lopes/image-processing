1d3dc5636a6f7a729c6dd05ab753c3223129ae5f
João Gabriel Lopes <jglopes6@gmail.com>
2020-01-15 10:08:13 +0100

    Corrected kmeans testing

--- classification.m
+++ classification.m
@@ -18,7 +18,7 @@ clc
 
 % We could only implement mean normalization, via something like X = X - mean(X);
 
-% However, applying a full Z normalization (standartization) is ideal,
+% However, applying a full Z normalization (standartization) is ideal,m_idx
 % since it will also normalize the stdev
 
 % However, in order for the normalization to be consistent between the
@@ -49,17 +49,72 @@ X = X * coeff(:, 1:n);
 
 % Reconstructing the separate X_train and X_test using the size of the
 % original matrices
-X_test = X(length(X_train)+1: end, :);
-X_train = X(1:length(X_train), :);
+index_X_train = length(X_train);
+X_test = X(index_X_train+1: end, :);
+X_train = X(1:index_X_train, :);
 
 
 
-% Creating a model using KNN
+% Creating a model using kmeans
+
+% 
+N_clusters = 15;
+[idx, centroids] = kmeans(X_train, N_clusters);
+
+% However notice that the centroid index in idx is not labeled the same way
+% as in the Y_train, therefore we need to have further data treatment
+base_truth = cell(1, N_clusters);
+for n = 1:length(idx)
+    base_truth{idx(n)} = [base_truth{idx(n)} Y_train(n)];
+end
+
+for n = 1:N_clusters
+    base_truth{n} = mode(base_truth{n});
+end
+
+for n = 1:length(idx)
+    idx(n) = base_truth{idx(n)};
+end
+
+result = idx == Y_train;
+fprintf("Kmeans training accuracy: %f\n", sum(result)/length(result));
+
+function idx = findClosestCentroids(X, centroids)
+
+    % findClosestCentroids computes the closest centroid for each point based
+    % on the Euclidean distance between the point and the centroid
+
+    % Initialize variables
+    K = size(centroids, 1); 
+    idx = zeros(size(X,1), 1); % returns index of closest centroid
+
+    for i=1:size(X,1)
+        temp = X(i,:);
+        [a,idx(i,1)] = min(sum(((bsxfun(@minus,temp,centroids)).^2),2));
+%         if i < 5
+%             a
+%         end
+    end
+
+end
+
+idx = findClosestCentroids(X_test, centroids);
+old_idx == idx
+
+for n = 1:length(idx)
+    idx(n) = base_truth{idx(n)};
+end
+
+result = idx == Y_test;
+fprintf("Kmeans testing accuracy: %f\n", sum(result)/length(result));
+
+
+% Creating a model using fitcknn
 mdl = fitcknn(X_train, Y_train', 'NumNeighbors', 5, 'Standardize',1);
 
 [label, ~, ~] = predict(mdl, X_train);
 result = Y_train == label;
-fprintf("Training accuracy: %f\n", sum(result)/length(result));
+fprintf("Fitcknn Training accuracy: %f\n", sum(result)/length(result));
 
 
 [label, ~, ~] = predict(mdl, X_test);
